e72d7c3b8607054a85b490c1a7f06cc9e3006ffb
Arseniy Khvorov <khvorov45@gmail.com>
2023-03-05 20:45:20 +1100

    do some more

--- hm.c
+++ hm.c
@@ -9,6 +9,7 @@ typedef prb_Arena Arena;
 typedef prb_Str   Str;
 typedef intptr_t  isize;
 typedef uint8_t   u8;
+typedef uint16_t  u16;
 
 function void
 execCmd(Arena* arena, Str cmd) {
@@ -24,45 +25,89 @@ readFile(Arena* arena, Str path) {
     return result;
 }
 
+function char*
+getRegStr(u8 reg, bool wbit) {
+    char* regTable16[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
+    char* regTable8[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
+    assert(prb_arrayCount(regTable8) == prb_arrayCount(regTable16));
+    assert(reg < prb_arrayCount(regTable16));
+
+    char** regTable = regTable16;
+    if (!wbit) {
+        regTable = regTable8;
+    }
+
+    char* result = regTable[reg];
+    return result;
+}
+
 function Str
 decode(Arena* arena, prb_Bytes input) {
-    assert(input.len > 0 && input.len % 2 == 0);
-
     prb_GrowingStr gstr = prb_beginStr(arena);
     prb_addStrSegment(&gstr, "bits 16\n");
 
-    for (isize offset = 0; offset < input.len; offset += 2) {
+    for (isize offset = 0; offset < input.len;) {
         u8 byte1 = input.data[offset];
-        u8 byte2 = input.data[offset + 1];
 
-        u8 movMask = 0b10001000;
-        assert((byte1 & movMask) == movMask);
+        if ((byte1 & 0b11111100) == 0b10001000) {
+            bool dbit = (byte1 & 0b10) != 0;
+            bool wbit = (byte1 & 0b1) != 0;
 
-        bool dbit = (byte1 & 0b10) != 0;
-        bool wbit = (byte1 & 0b1) != 0;
+            u8 byte2 = input.data[offset + 1];
+            u8 mod = byte2 >> 6;
 
-        u8 mod = byte2 >> 6;
-        assert(mod == 0b11);
+            switch (mod) {
+                case 0b00: {
+                    assert(!"unimplemented");
+                } break;
 
-        u8 reg = (byte2 & 0b00111000) >> 3;
-        u8 rm = byte2 & 0b00000111;
+                case 0b01: {
+                    assert(!"unimplemented");
+                } break;
 
-        u8 destReg = reg;
-        u8 srcReg = rm;
-        if (!dbit) {
-            destReg = rm;
-            srcReg = reg;
+                case 0b10: {
+                    assert(!"unimplemented");
+                } break;
+
+                case 0b11: {
+                    u8 reg = (byte2 & 0b00111000) >> 3;
+                    u8 rm = byte2 & 0b00000111;
+
+                    u8 destReg = reg;
+                    u8 srcReg = rm;
+                    if (!dbit) {
+                        destReg = rm;
+                        srcReg = reg;
+                    }
+
+                    prb_addStrSegment(&gstr, "mov %s, %s\n", getRegStr(destReg, wbit), getRegStr(srcReg, wbit));
+                    offset += 2;
+                } break;
+
+                default: assert(!"unreachable"); break;
+            }
+        } else if ((byte1 & 0b11111110) == 0b11000110) {
+            assert(!"unimplemented");
+        } else if ((byte1 & 0b11110000) == 0b10110000) {
+            bool wbit = (byte1 & 0b1000) != 0;
+            u16  value = (u16)input.data[offset + 1];
+            if (wbit) {
+                u16 byte3 = input.data[offset + 2];
+                value = value | (byte3 << 8);
+            }
+
+            u8 reg = byte1 & 0b111;
+            prb_addStrSegment(&gstr, "mov %s, %d\n", getRegStr(reg, wbit), value);
+
+            offset += 2;
+            if (wbit) {
+                offset += 1;
+            }
+        } else {
+            assert(!"unimplemented");
         }
 
-        char* regTable16[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
-        char* regTable8[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
-
-        char** regTable = regTable16;
-        if (!wbit) {
-            regTable = regTable8;
-        }
-
-        prb_addStrSegment(&gstr, "mov %s, %s\n", regTable[destReg], regTable[srcReg]);
+        assert(offset <= input.len);
     }
 
     Str result = prb_endStr(&gstr);
@@ -111,6 +156,27 @@ main() {
             "mov bx, si\n"
             "mov sp, di\n"
             "mov bp, ax\n"
+        )
+    );
+
+    test_decode(
+        arena,
+        STR(
+            "bits 16\n"
+            "mov si, bx\n"
+            "mov dh, al\n"
+            "mov cx, 12\n"
+            "mov cx, -12\n"
+            "mov dx, 3948\n"
+            "mov dx, -3948\n"
+            // "mov al, [bx + si]\n"
+            // "mov bx, [bp + di]\n"
+            // "mov dx, [bp]\n"
+            // "mov ah, [bx + si + 4]\n"
+            // "mov al, [bx + si + 4999]\n"
+            // "mov [bx + di], cx\n"
+            // "mov [bp + si], cl\n"
+            // "mov [bp], ch\n"
         )
     );
 
