18b19e398732248fb906467049eba7aefc5ffb2a
Arseniy Khvorov <khvorov45@gmail.com>
2023-03-06 17:49:27 +1100

    do more

--- hm.c
+++ hm.c
@@ -10,6 +10,7 @@ typedef prb_Str   Str;
 typedef intptr_t  isize;
 typedef uint8_t   u8;
 typedef uint16_t  u16;
+typedef int16_t   i16;
 
 function void
 execCmd(Arena* arena, Str cmd) {
@@ -47,7 +48,8 @@ decode(Arena* arena, prb_Bytes input) {
     prb_addStrSegment(&gstr, "bits 16\n");
 
     for (isize offset = 0; offset < input.len;) {
-        u8 byte1 = input.data[offset];
+        u8    byte1 = input.data[offset];
+        char* eqs[] = {"bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx"};
 
         if ((byte1 & 0b11111100) == 0b10001000) {
             bool dbit = (byte1 & 0b10) != 0;
@@ -58,39 +60,53 @@ decode(Arena* arena, prb_Bytes input) {
             u8 reg = (byte2 & 0b00111000) >> 3;
             u8 rm = byte2 & 0b00000111;
 
-            char* eqs[] = {"bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx"};
-
             char* regStr = getRegStr(reg, wbit);
             char* eqStr = eqs[rm];
 
             switch (mod) {
                 case 0b00: {
-                    assert(rm != 0b110);
+                    if (rm == 0b110) {
+                        u8  byte3 = input.data[offset + 2];
+                        u8  byte4 = input.data[offset + 3];
+                        u16 address = ((u16)byte4 << 8) | (u16)byte3;
 
-                    if (dbit) {
-                        prb_addStrSegment(&gstr, "mov %s, [%s]\n", regStr, eqStr);
+                        if (dbit) {
+                            prb_addStrSegment(&gstr, "mov %s, [%d]\n", regStr, address);
+                        } else {
+                            prb_addStrSegment(&gstr, "mov [%d], %s,\n", address, regStr);
+                        }
+
+                        offset += 4;
                     } else {
-                        prb_addStrSegment(&gstr, "mov [%s], %s\n", eqStr, regStr);
-                    }
+                        if (dbit) {
+                            prb_addStrSegment(&gstr, "mov %s, [%s]\n", regStr, eqStr);
+                        } else {
+                            prb_addStrSegment(&gstr, "mov [%s], %s\n", eqStr, regStr);
+                        }
 
-                    offset += 2;
+                        offset += 2;
+                    }
                 } break;
 
                 case 0b01: {
-                    u8 byte3 = input.data[offset + 2];
+                    u8  byte3 = input.data[offset + 2];
+                    i16 val = byte3;
+                    if (byte3 & 0b10000000) {
+                        val = 0b1111111100000000 | (u16)byte3;
+                    }
 
                     if (dbit) {
-                        prb_addStrSegment(&gstr, "mov %s, [%s + %d]\n", regStr, eqStr, byte3);
+                        prb_addStrSegment(&gstr, "mov %s, [%s + %d]\n", regStr, eqStr, val);
                     } else {
-                        prb_addStrSegment(&gstr, "mov [%s + %d], %s\n", eqStr, byte3, regStr);
+                        prb_addStrSegment(&gstr, "mov [%s + %d], %s\n", eqStr, val, regStr);
                     }
 
                     offset += 3;
                 } break;
 
                 case 0b10: {
-                    u8 byte3 = input.data[offset + 2];
-                    u8 byte4 = input.data[offset + 3];
+                    u8  byte3 = input.data[offset + 2];
+                    u8  byte4 = input.data[offset + 3];
                     u16 val = ((u16)byte4 << 8) | (u16)byte3;
 
                     if (dbit) {
@@ -117,7 +133,71 @@ decode(Arena* arena, prb_Bytes input) {
                 default: assert(!"unreachable"); break;
             }
         } else if ((byte1 & 0b11111110) == 0b11000110) {
-            assert(!"unimplemented");
+            bool wbit = (byte1 & 0b1) != 0;
+
+            u8 byte2 = input.data[offset + 1];
+
+            u8 mod = byte2 >> 6;
+
+            u8 reg = (byte2 & 0b00111000) >> 3;
+            assert(reg == 0);
+
+            u8    rm = byte2 & 0b00000111;
+            char* eqStr = eqs[rm];
+
+            switch (mod) {
+                case 0b00: {
+                    assert(rm != 0b110);
+                    u8    byte3 = input.data[offset + 2];
+                    u16   val = byte3;
+                    char* size = "byte";
+                    if (wbit) {
+                        u8 byte4 = input.data[offset + 3];
+                        val = ((u16)byte4 << 8) | val;
+                        size = "word";
+                    }
+
+                    prb_addStrSegment(&gstr, "mov [%s], %s %d\n", eqStr, size, val);
+
+                    offset += 3;
+                    if (wbit) {
+                        offset += 1;
+                    }
+                } break;
+
+                case 0b01: {
+                    assert(!"unimplemented");
+                } break;
+
+                case 0b10: {
+                    u8  byte3 = input.data[offset + 2];
+                    u8  byte4 = input.data[offset + 3];
+                    u16 disp = ((u16)byte4 << 8) | (u16)byte3;
+
+                    u8    byte5 = input.data[offset + 4];
+                    u16   val = byte5;
+                    char* size = "byte";
+                    if (wbit) {
+                        u8 byte6 = input.data[offset + 5];
+                        val = ((u16)byte6 << 8) | val;
+                        size = "word";
+                    }
+
+                    prb_addStrSegment(&gstr, "mov [%s + %d], %s %d\n", eqStr, disp, size, val);
+
+                    offset += 5;
+                    if (wbit) {
+                        offset += 1;
+                    }
+                } break;
+
+                case 0b11: {
+                    assert(!"unimplemented");
+                } break;
+
+                default: assert(!"unreachable"); break;
+            }
+
         } else if ((byte1 & 0b11110000) == 0b10110000) {
             bool wbit = (byte1 & 0b1000) != 0;
             u16  value = (u16)input.data[offset + 1];
@@ -129,6 +209,34 @@ decode(Arena* arena, prb_Bytes input) {
             u8 reg = byte1 & 0b111;
             prb_addStrSegment(&gstr, "mov %s, %d\n", getRegStr(reg, wbit), value);
 
+            offset += 2;
+            if (wbit) {
+                offset += 1;
+            }
+        } else if ((byte1 & 0b11111110) == 0b10100000) {
+            bool wbit = (byte1 & 0b1) != 0;
+            u8   byte2 = input.data[offset + 1];
+            u16  address = byte2;
+            if (wbit) {
+                u8 byte3 = input.data[offset + 2];
+                address = ((u16)byte3 << 8) | address;
+            }
+
+            prb_addStrSegment(&gstr, "mov ax, [%d]\n", address);
+            offset += 2;
+            if (wbit) {
+                offset += 1;
+            }
+        } else if ((byte1 & 0b11111110) == 0b10100010) {
+            bool wbit = (byte1 & 0b1) != 0;
+            u8   byte2 = input.data[offset + 1];
+            u16  address = byte2;
+            if (wbit) {
+                u8 byte3 = input.data[offset + 2];
+                address = ((u16)byte3 << 8) | address;
+            }
+
+            prb_addStrSegment(&gstr, "mov [%d], ax\n", address);
             offset += 2;
             if (wbit) {
                 offset += 1;
@@ -199,6 +307,17 @@ main() {
             "mov [bx + di], cx\n"
             "mov [bp + si], cl\n"
             "mov [bp], ch\n"
+            "mov ax, [bx + di - 37]\n"
+            "mov [si - 300], cx\n"
+            "mov dx, [bx - 32]\n"
+            "mov [bp + di], byte 7\n"
+            "mov [di + 901], word 347\n"
+            "mov bp, [5]\n"
+            "mov bx, [3458]\n"
+            "mov ax, [2555]\n"
+            "mov ax, [16]\n"
+            "mov [2554], ax\n"
+            "mov [15], ax\n"
         )
     );
 
