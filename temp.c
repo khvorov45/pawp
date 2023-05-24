169e753d441519f18a77b4082964be8f34980a2d
Arseniy Khvorov <khvorov45@gmail.com>
2023-05-24 20:34:18 +1000

    remove temp file

--- temp.c
+++
@@ -1,4 +0,0 @@
-loop(Loop)                                  | 11100010 | ip_inc8
-loopz(Loop)                                 | 11100001 | ip_inc8
-loopnz(Loop)                                | 11100000 | ip_inc8
-jcxz(Jump)                                  | 11100011 | ip_inc8
